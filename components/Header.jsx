1a4eccb5750de6c5eb7a6a98963b5a3f812d9c82
0m4rca <omarconalf@gmail.com>
2025-07-16 23:48:25 -0700

    cover-img

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,18 +10,38 @@ export default function Header() {
 
   return (
     <header className="relative h-screen w-full overflow-hidden">
-      <Image
-        src="/cover.jpg"
-        alt="Scuba Diving Adventure"
-        fill
-        priority
-        className="object-cover"
-      />
+      <div className="relative h-screen w-full overflow-hidden">
+        {/* Imagen móvil */}
+        <Image
+          src="/cover-mobile.jpg"
+          alt="Scuba Diving Adventure"
+          fill
+          priority
+          className="object-cover block md:hidden"
+        />
+
+        {/* Imagen desktop */}
+        <Image
+          src="/cover-desktop.jpg"
+          alt="Scuba Diving Adventure"
+          fill
+          priority
+          className="object-cover hidden md:block object-[center_90%]"
+        />
+      </div>
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/40 z-10"></div>
 
       {/* Content */}
-      <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-white text-center px-6">
+      <div
+        className="
+      absolute 
+      inset-0 z-20 
+      flex flex-col items-center justify-center 
+      text-white text-center 
+      px-6 translate-y-20
+      gap-2 md:gap-4"
+      >
         <HeadingPrimary
           main="Dive with VIISOR"
           sub="Witness Baja California Sur"
