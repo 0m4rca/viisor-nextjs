1b69a3513f52b726568253e0941b0450046ddfc4
0m4rca <omarconalf@gmail.com>
2025-07-22 20:28:55 -0700

    image2

--- app/layout.jsx
+++ app/layout.jsx
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`md:px-8 md:py-6 lg:px-16 lg:py-10 ${montserrat.variable} font-sans bg-secondary text-tertiary`}
+        className={`md:px-6 md:py-6 lg:px-10 lg:py-10 ${montserrat.variable} font-sans bg-secondary text-tertiary`}
       >
         <div className="relative">
           <Navigation />
--- components/Header.jsx
+++ components/Header.jsx
@@ -9,25 +9,26 @@ export default function Header() {
 
   return (
     <header className="relative h-screen w-full overflow-hidden">
-      <div className="relative h-screen w-full overflow-hidden">
-        {/* Imagen móvil */}
-        <Image
-          src="/cover-mobile.JPG"
-          alt="Scuba Diving Adventure"
-          fill
-          priority
-          className="object-cover block lg:hidden"
-        />
+      {/* Imagen móvil */}
+      <Image
+        src="/cover-mobile.JPG"
+        alt="Scuba Diving Adventure"
+        fill
+        priority
+        sizes="(max-width: 1279px) 100vw, 0px"
+        className="object-cover block 2xl:hidden object-[center_40%]"
+      />
 
-        {/* Imagen desktop */}
-        <Image
-          src="/cover-desktop.JPG"
-          alt="Scuba Diving Adventure"
-          fill
-          priority
-          className="object-cover hidden lg:block object-[center_90%]"
-        />
-      </div>
+      {/* Imagen desktop */}
+      <Image
+        src="/cover-desktop.JPG"
+        alt="Scuba Diving Adventure"
+        fill
+        priority
+        sizes="(min-width: 1280px) 100vw, 0px"
+        className="object-cover hidden 2xl:block object-[center_90%]"
+      />
+
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/40 z-10"></div>
 
@@ -41,17 +42,27 @@ export default function Header() {
       px-6 translate-y-20
       gap-2 md:gap-4"
       >
-        <HeadingPrimary
-          main="Dive with VIISOR"
-          sub="Witness Baja California Sur"
-        />
-        <ButtonText
-          href="/Tours"
-          className=" bg-[var(--color-secondary-dark)] text-gray-700
+        <div
+          className="
+        flex 
+        flex-col 
+        items-center 
+        text-center 
+        mt-8
         "
         >
-          Discover our tours
-        </ButtonText>
+          <HeadingPrimary
+            main="Dive with VIISOR"
+            sub="Witness Baja California Sur"
+          />
+          <ButtonText
+            href="/Tours"
+            className=" bg-[var(--color-secondary-dark)] text-gray-700
+        "
+          >
+            Discover our tours
+          </ButtonText>
+        </div>
       </div>
     </header>
   );
